094802f1fa78a83353dd140436446af3ecfc2624
Luis Ake <luis.ake142002@gmail.com>
2024-11-09 23:32:31 -0600

    crud_habitos

---
+++ lib/components/my_habit_tile.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
+
+class MyHabitTile extends StatelessWidget {
+  final String text;
+  final bool isCompleted;
+  final void Function(bool?)? onChanged;
+  final void Function(BuildContext)? editHabit;
+    final void Function(BuildContext)? deleteHabit;
+
+  const MyHabitTile({
+    super.key,
+    required this.text,
+    required this.isCompleted,
+    required this.onChanged,
+    required this.editHabit,
+    required this.deleteHabit,
+    });
+
+  @override
+  Widget build(BuildContext context) {
+    return Slidable(
+      endActionPane: ActionPane(
+        motion: const StretchMotion(),
+        children: [
+          //editar opcion
+          SlidableAction(
+            onPressed: editHabit,
+            backgroundColor: Colors.grey.shade800,
+            icon: Icons.settings,
+            borderRadius: BorderRadius.circular(8),
+          ),
+
+          //borrar opcion
+          SlidableAction(
+            onPressed: deleteHabit,
+            backgroundColor: Colors.red,
+            icon: Icons.delete,
+            borderRadius: BorderRadius.circular(8),
+          ),
+        ],
+      ),
+      child: GestureDetector(
+        onTap: (){
+          if(onChanged != null){
+            //cambiar el estado de completado
+            onChanged!(!isCompleted);
+          }
+        },
+        child: Container(
+          decoration: BoxDecoration(
+            color: isCompleted 
+              ? Colors.green 
+              : Theme.of(context).colorScheme.secondary,
+            borderRadius: BorderRadius.circular(8),
+          ),
+          padding: const EdgeInsets.all(12),
+          margin: const EdgeInsets.symmetric(vertical: 5, horizontal: 25),
+          child: ListTile(
+            title: Text(text),
+            leading: Checkbox(
+              activeColor: Colors.green,
+              value: isCompleted,
+              onChanged: onChanged,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
 import 'package:minimal_habit_tracker_luis_ake/components/my_drawer.dart';
+import 'package:minimal_habit_tracker_luis_ake/components/my_habit_tile.dart';
 import 'package:minimal_habit_tracker_luis_ake/database/habit_database.dart';
 import 'package:minimal_habit_tracker_luis_ake/models/habit.dart';
 import 'package:minimal_habit_tracker_luis_ake/util/habit_util.dart';
@@ -71,6 +71,97 @@ class _HomePageState extends State<HomePage> {
     );
   }
 
+  // marcar el habito si o no
+  void checkHabitOnOff(bool? value, Habit habit) {
+    if (value != null) {
+      context.read<HabitDatabase>().updateHabitCompletion(habit.id, value);
+    }
+  }
+
+
+    //caja de editar habito
+    void editHabitBox(Habit habit){
+      //definir el controlador del texto del habito actual
+      textControler.text = habit.name;
+
+      showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+          content: TextField(
+            controller: textControler,
+          ),
+          actions: [
+            //boton guardar
+            MaterialButton(
+              onPressed: (){
+                //guardar el nuevo habito
+                String newHabitName = textControler.text;
+
+                //guaradar en la base de datos
+                context
+                  .read<HabitDatabase>()
+                  .updateHabitName(habit.id, newHabitName);
+
+                //caja pop
+                Navigator.pop(context);
+
+                //limpiar
+                textControler.clear();
+            },
+            child: const Text('guardar'),
+            ),
+
+            //boton cancelar
+            MaterialButton(onPressed: (){
+              //cerrar la caja
+              Navigator.pop(context);
+
+              //limpiar
+              textControler.clear();
+            },
+          child: const Text('Cancelar'),
+          )
+          ],
+      ),
+    );
+  }
+
+  //caja de borrar habito
+  void deleteHabitBox(Habit habit){
+    //definir el controlador del texto del habito actual
+      textControler.text = habit.name;
+
+      showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+         title: const Text('Estas seguro que quieres borrar?'),
+          actions: [
+            //boton borrar
+            MaterialButton(
+              onPressed: (){
+                
+                //guaradar en la base de datos
+                context.read<HabitDatabase>().deleteHabit(habit.id);
+
+                //caja pop
+                Navigator.pop(context);
+            },
+            child: const Text('Borrar'),
+            ),
+
+            //boton cancelar
+            MaterialButton(onPressed: (){
+              //cerrar la caja
+              Navigator.pop(context);
+
+            },
+          child: const Text('Cancelar'),
+          )
+          ],
+      ),
+    );
+  }
+
 
   @override
   Widget build(BuildContext context) {
@@ -109,8 +200,12 @@ class _HomePageState extends State<HomePage> {
         bool isCompletedToday = isHabitCompletedToday(habit.completedDays);
 
         //devolver el habito a la interfaz
-        return ListTile(
-          title: Text(habit.name),
+        return MyHabitTile(
+          text: habit.name,
+          isCompleted: isCompletedToday,
+          onChanged: (value) => checkHabitOnOff(value, habit),
+          editHabit: (context) => editHabitBox(habit),
+          deleteHabit: (context) => deleteHabitBox(habit),
         );
       }
     );
